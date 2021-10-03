3d2ecb8313d7b9599c2c88c876c36aa72ccd32ab
MelarcAB <melarc.ab@gmail.com>
2021-10-03 19:55:25 +0200

    Change commands arr to commands object with more properties Code optimization Adding author info in command

--- discordHelper.js
+++ discordHelper.js
@@ -2,6 +2,10 @@ const config = require("./config.json");
 const Discord = require('discord.js');
 const app = require("./app.js");
 const fs = require('fs')
+const {
+    MessageEmbed
+} = require('discord.js');
+
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
 })
@@ -18,8 +22,9 @@ client.on('messageCreate', async msg => {
     var msg_content = (msg.content)
     if (!msg_content.startsWith("!")) return;
     if (!helper.isCommand(msg_content)) return;
-
     let functionCommand = helper.getFunctionCommand(msg_content.replace("!", "").split(/[ ,]+/)[0]);
+    if (!functionCommand) return;
+
 
     //Call function using functionCommand
     storedFunctions[functionCommand](msg)
@@ -29,8 +34,8 @@ client.on('messageCreate', async msg => {
 });
 
 
-
-storedFunctions.initGenerateCard= async function (msg){
+//Functions
+storedFunctions.initGenerateCard = async function (msg) {
     let data = cardHelper.getDataFromMessage(msg)
     let generated_card = await cardHelper.generateCardImg(data)
     let path = './tmp_files/' + data.discord_nick + data.discord_discriminator + ".jpeg"
@@ -39,12 +44,36 @@ storedFunctions.initGenerateCard= async function (msg){
     });
 
     fs.unlinkSync(path);
-    console.log(">Card removed : "+path)
+    console.log(">Card removed : " + path)
+}
+
+storedFunctions.showHelpCommands = async function (msg) {
+    /* await msg.channel.send({
+         files: [path]
+     });*/
 }
 
 
+storedFunctions.showBotInfo = async function (msg) {
 
 
+
+}
+
+storedFunctions.showAuthorInfo = function (msg) {
+
+    const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Melarc#1006')
+        .setURL('https://melarcab.com/')
+        .setAuthor('Melarc AB', 'https://cdn.discordapp.com/avatars/141909410019540992/7f0b7bb5849c41f6cc6ea63975856a30.jpeg', 'https://discordapp.com/users/141909410019540992')
+        .setDescription('Web developer and digital artist.')
+        .setThumbnail('https://cdn.discordapp.com/avatars/141909410019540992/7f0b7bb5849c41f6cc6ea63975856a30.jpeg')
+        .setFooter('Visita mi página web', 'https://cdn.discordapp.com/avatars/141909410019540992/7f0b7bb5849c41f6cc6ea63975856a30.jpeg');
+    msg.channel.send({
+        embeds: [exampleEmbed]
+    });
+}
 
 
 
