6d8f617e3e45b3b27e3b55783e50474f1a1100ec
MelarcAB <melarc.ab@gmail.com>
2021-10-03 02:46:24 +0200

    Remove unnecessary code

--- app.js
+++ app.js
@@ -3,28 +3,11 @@ const Discord = require('discord.js');
 const fs = require("fs");
 const nodeHtmlToImage = require('node-html-to-image')
 
-//Exports
+//Custom modules
 var discordHelper = require("./discordHelper.js");
 
-
-//Vars
-
-
-
-
-//Init
+//Init bot
 discordHelper.startBot();
-
-//Events
-
-
-
-
-
-
-
-
-
 
 function getDiscordClient(){
     return client;
@@ -34,58 +17,3 @@ function getDiscordClient(){
 module.exports = {
     getDiscordClient: getDiscordClient,
 }
-
-//const client = new Discord.Client();
-
-
-
-
-
-//Init bot discord
-
-//client.login(config.BOT_TOKEN);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-//Función para generar imagen a partir de la plantila
-function generateCardImg() {
-    let template_html = fs.readFileSync(__dirname + '/templates/card_template.html', 'utf8');
-    let css_content = fs.readFileSync(__dirname + '/templates/style.css', 'utf8');
-
-    let vars_render = {
-        css_content: css_content,
-        discord_nick :"Melarc#1006",
-        name: "MelarcAB",
-        description : "Desarrollador web/Artista. Un genio entre genios.",
-        img : ""
-    }
-
-    nodeHtmlToImage({
-            output: './test.jpeg',
-            html: template_html,
-            content: vars_render
-        })
-        .then(() => console.log('The image was created successfully! :O'))
-}
-
-generateCardImg();
-
-
-*/
